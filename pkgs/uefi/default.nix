a57333f2f499264d2d48132d9d8bfde454fce462
Yureka <yuka@yuka.dev>
2026-03-09 14:02:39 +0100

    fix truncate being ignored

--- pkgs/uefi/default.nix
+++ pkgs/uefi/default.nix
@@ -6,7 +6,7 @@ let
 in
 runCommand "lx2k-firmware-${target}.bin" { } ''
   truncate -s 8M $out
-  dd of=$out bs=512 if=${atf-with-tianocore}/lx2160acex7/bl2_sd.pbl seek=8
+  dd of=$out bs=512 if=${atf-with-tianocore}/lx2160acex7/bl2_sd.pbl seek=8 conv=notrunc
   dd of=$out bs=512 if=${ddr-phy-bin}/fip_ddr_all.bin seek=256 conv=notrunc
   dd of=$out bs=512 if=${atf-with-tianocore}/lx2160acex7/fip.bin seek=2048 conv=notrunc
 ''
